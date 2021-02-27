556c6a9cafe49feb40b303f27273f401f293deb2
guivictorr <guilhermevictor20@gmail.com>
2021-02-27 18:36:26 -0300

    chore(config): configured intial structure for start

---
+++ pages/_document.tsx
@@ -0,0 +1,14 @@
+import Document, { Html, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
