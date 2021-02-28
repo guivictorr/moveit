a9b18d35297b4db4ce2beecaf546ae74f8d21a01
guivictorr <guilhermevictor20@gmail.com>
2021-02-27 21:23:10 -0300

    feat(completedchallenges): create completed challenges component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import styles from '../src/styles/pages/Home.module.scss';
 
 import ExperienceBar from '../src/components/ExperienceBar';
 import Profile from '../src/components/Profile';
+import CompletedChallenges from '../src/components/CompletedChallenges';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <section>
         <div className={styles.leftContainer}>
           <Profile />
+          <CompletedChallenges />
         </div>
         <div />
       </section>
