09c266de4a3667b5e39c6206010a0f1d1714956c
guivictorr <guilhermevictor20@gmail.com>
2021-02-27 21:08:32 -0300

    feat(profile): created profile component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
-import styles from '../src/styles/components/Home.module.scss';
+import styles from '../src/styles/pages/Home.module.scss';
+
 import ExperienceBar from '../src/components/ExperienceBar';
+import Profile from '../src/components/Profile';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <ExperienceBar />
+
+      <section>
+        <div className={styles.leftContainer}>
+          <Profile />
+        </div>
+        <div />
+      </section>
     </div>
   );
 }
